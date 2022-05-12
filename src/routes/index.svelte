e5a40fff834b811cafb5a1070fe4f7c7abecce8a
John Foster <john@spin-up.io>
2022-05-12 22:14:38 +0100

    removes nav, adds container

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+	// script goes here
 </script>
 
-<main class="flex flex-col">
-	<Nav />
+<main class="container mx-auto">
 	<div class="text-center text-blue-600">
-		<h1 class="text-3xl font-bold mb-2">Welcome to SvelteKit x Tailwind</h1>
+		<h1 class="text-3xl font-bold">Welcome to SvelteKit x Tailwind</h1>
+		<span>~</span>
 		<h3 class="font-bold">a SpinUp Labs production</h3>
 	</div>
 </main>
