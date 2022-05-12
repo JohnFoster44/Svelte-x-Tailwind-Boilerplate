dc9cf23f468640b8ba536e409b2f1f07208a6783
John Foster <john@spin-up.io>
2022-05-12 22:14:18 +0100

    snazzes up the about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,1 +1,11 @@
-<h1>About page simple</h1>
+<script>
+	// script goes here
+</script>
+
+<main class="container mx-auto">
+	<div class="text-center text-blue-600">
+		<h1 class="text-3xl font-bold">About page</h1>
+		<span>~</span>
+		<h3 class="font-bold">a simple page nothing much to see here</h3>
+	</div>
+</main>
