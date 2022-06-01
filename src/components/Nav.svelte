d994f311a1746f31a3f923f8bdee435b10e8b537
John Foster <john@spin-up.io>
2022-06-01 11:59:38 +0100

    Adds contact page, margin to nav items

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -8,10 +8,10 @@
 <nav>
     <div class="navbar">
         <a href="/" class="font-bold text-xl text-green-300 | hover:text-green-200 transition">SpinUp Labs x Svelte/TW</a>
-        
+        <!-- DT menu -->
         <ul class="hidden lg:flex">
             {#each menu as item}
-            <li class="mx-1"><a href="about" class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
+            <li class="mx-1"><a href={item.url} class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
             {/each}
         </ul>
     
@@ -22,10 +22,10 @@
                 <span class="line line-2"></span>
                 <span class="line line-3"></span>
             </div>
-    
+            <!-- Mobile menu -->
             <ul class="menu-items">
                 {#each menu as item}
-                <li class="mx-1"><a href="about" class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
+                <li class="mx-1 mb-5"><a href={item.url} class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
                 {/each}
             </ul>
         </div>
---
+++ src/routes/contact.svelte
@@ -0,0 +1,12 @@
+<script>
+	// script goes here
+</script>
+
+<main class="container mx-auto">
+	<div class="text-center text-blue-600">
+		<h1 class="text-3xl font-bold">Contact</h1>
+		<span>~</span>
+		<h3>This is SpinUp</h3>
+		<img src="/images/SpinUp.jpg" alt="">
+	</div>
+</main>
