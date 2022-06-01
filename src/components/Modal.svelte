a2ad5cc543335132df8a5806cf7cd5d8d807801b
John Foster <john@spin-up.io>
2022-06-01 14:09:19 +0100

    Adds my Modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,15 @@
+<script>
+    let shown = false;
+    export function toggle(){
+        shown = !shown
+    }
+</script>
+
+{#if shown}
+<div class="bg-blue-500 bg-opacity-50 fixed w-full h-full top-0 left-0 text-center text-blue-600">
+    <div class="border-2 border-blue-500 rounded-2xl bg-slate-100 max-w-4xl p-5 mx-auto my-20">
+        <button on:click={() => toggle()} class="rounded-full border border-blue-500 px-3.5 py-2 font-extrabold text-lg transition | hover:bg-blue-500 hover:text-green-300">&#10005;</button>
+        <slot>Modal Content goes here</slot>
+    </div>
+</div>
+{/if}
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,5 +1,6 @@
 <script>
-	// script goes here
+	import Modal from "../components/Modal.svelte";
+	let modal
 </script>
 
 <main class="container mx-auto">
@@ -7,5 +8,14 @@
 		<h1 class="text-3xl font-bold">About page</h1>
 		<span>~</span>
 		<h3 class="font-bold">a simple page nothing much to see here</h3>
+		<button class="border p-5 bg-blue-600 text-green-300 font-bold text-lg" on:click={() => modal.toggle()}>Modal</button>
 	</div>
 </main>
+
+<Modal bind:this={modal}>
+	<h1 class="text-3xl font-bold">Modal</h1>
+	<span>~</span>
+	<p>
+		boo yahhh
+	</p>
+</Modal>
