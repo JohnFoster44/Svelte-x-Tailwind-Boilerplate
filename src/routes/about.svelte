660425df865b8c1fa6876aa732c92dabedfea594
John Foster <john@spin-up.io>
2022-06-01 15:40:31 +0100

    Modal Stylings

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -7,7 +7,7 @@
 	<div class="text-center text-blue-600">
 		<h1 class="text-3xl font-bold">About page</h1>
 		<span>~</span>
-		<h3 class="font-bold">a simple page nothing much to see here</h3>
+		<h3 class="font-bold mb-5">a simple page nothing much to see here</h3>
 		<button class="border p-5 bg-blue-600 text-green-300 font-bold text-lg" on:click={() => modal.toggle()}>Modal</button>
 	</div>
 </main>
