0abf46b9c7552cb84da75e23f622f2be2471aa82
John Foster <john@spin-up.io>
2022-06-01 11:48:21 +0100

    Finishing touches to nav: switched raw css to TW in styles.css

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,14 +1,33 @@
-<nav class="p-2 mb-5 bg-blue-600 flex justify-between | navbar">
-    <a href="/" class="font-bold text-green-300 | hover:text-green-200 transition">SpinUp Labs x Svelte/TW</a>
-    <ul>
-        <li><a href="about" class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">about</a></li>
-    </ul>
-    <div class="burger-container">
-        <input type="checkbox" class="checkbox"/>
-        <div class="burger">
-            <span class="line line-1"></span>
-            <span class="line line-2"></span>
-            <span class="line line-3"></span>
+<script>
+    let menu = [
+        { name: 'About', url: '/about' },
+        { name: 'Contact', url: '/contact'}
+    ];
+</script>
+
+<nav>
+    <div class="navbar">
+        <a href="/" class="font-bold text-green-300 | hover:text-green-200 transition">SpinUp Labs x Svelte/TW</a>
+        
+        <ul class="hidden lg:flex">
+            {#each menu as item}
+            <li class="mx-1"><a href="about" class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
+            {/each}
+        </ul>
+    
+        <div class="burger-container">
+            <input type="checkbox" class="checkbox"/>
+            <div class="burger">
+                <span class="line line-1"></span>
+                <span class="line line-2"></span>
+                <span class="line line-3"></span>
+            </div>
+    
+            <ul class="menu-items">
+                {#each menu as item}
+                <li class="mx-1"><a href="about" class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
+                {/each}
+            </ul>
         </div>
     </div>
 </nav>
