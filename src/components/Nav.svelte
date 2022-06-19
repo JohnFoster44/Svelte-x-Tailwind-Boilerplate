43cc347e2e3dd3872686bef073b6558d317cdde6
John Foster <john@spin-up.io>
2022-06-20 00:28:35 +0100

    Simplified navbar, inc the menu items

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,35 +1,57 @@
 <script>
-    let menu = [
-        { name: 'About', url: '/about' },
-        { name: 'Contact', url: '/contact'}
-    ];
+	import BurgerMenu from '../components/BurgerMenu.svelte';
+	import { fly } from 'svelte/transition';
+	let menu = [
+		{ name: 'About', url: '/about' },
+		{ name: 'Contact', url: '/contact' }
+	];
+	let open;
 </script>
 
-<nav>
-    <div class="navbar">
-        <a href="/" class="font-bold text-xl text-green-300 | hover:text-green-200 transition">SpinUp Labs x Svelte/TW</a>
-        <!-- DT menu -->
-        <ul class="hidden lg:flex">
-            {#each menu as item}
-            <li class="mx-1"><a href={item.url} class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
-            {/each}
-        </ul>
-    
-        <div class="burger-container">
-            <input type="checkbox" class="checkbox"/>
-            <div class="burger">
-                <span class="line line-1"></span>
-                <span class="line line-2"></span>
-                <span class="line line-3"></span>
-            </div>
-            <!-- Mobile menu -->
-            <div class="menu-items">
-                <ul>
-                    {#each menu as item}
-                    <li class="mx-1 mb-5"><a href={item.url} class="font-bold text-green-300 | hover:underline hover:text-green-200 transition">{item.name}</a></li>
-                    {/each}
-                </ul>
-            </div>
-        </div>
-    </div>
-</nav>
+<header>
+	<div class="navbar">
+		<a href="/" class="font-bold text-xl text-green-300 | hover:text-green-200 transition"
+			>SpinUp Labs x Svelte/TW</a
+		>
+		<!-- DT menu -->
+		<nav>
+			<ul class="hidden lg:flex">
+				{#each menu as item}
+					<li class="mx-1">
+						<a
+							href={item.url}
+							class="font-bold text-green-300 | hover:underline hover:text-green-200 transition"
+							>{item.name}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+		<BurgerMenu bind:open />
+	</div>
+	{#if open}
+		<nav
+			class="bg-blue-600 absolute top-0 h-screen w-full text-center text-4xl overflow-hidden pt-10"
+			transition:fly={{ x: 150, duration: 150 }}
+		>
+			<ul>
+				<li class="mb-5">
+					<a
+						href="/"
+						class="font-bold text-green-300 | hover:underline hover:text-green-200 transition"
+						on:click={() => (open = !open)}>Home</a
+					>
+				</li>
+				{#each menu as item}
+					<li class="mb-5">
+						<a
+							href={item.url}
+							class="font-bold text-green-300 | hover:underline hover:text-green-200 transition"
+							on:click={() => (open = !open)}>{item.name}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</nav>
+	{/if}
+</header>
