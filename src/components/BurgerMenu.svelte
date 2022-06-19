ee5070a79ff883ce4a566564493eebb47c28e5b3
John Foster <john@spin-up.io>
2022-06-19 23:51:43 +0100

    Burger button simplify

---
+++ src/components/BurgerMenu.svelte
@@ -0,0 +1,24 @@
+<script>
+	//  script for burger menu
+	export let open = false;
+	export let onClick = () => {
+		open = !open;
+		console.log(open);
+	};
+	export let ariaLabel = 'toggle menu';
+</script>
+
+<button
+	on:click={onClick}
+	aria-expanded={open}
+	aria-label={ariaLabel}
+	class="text-4xl w-10 h-10 flex justify-center items-center | lg:hidden"
+>
+	{#if open}
+		<span class="text-green-300 hover:text-green-200 text-3xl"> &#10005; </span>
+	{:else}
+		<span class="text-green-300 hover:text-green-200">
+			<img src="/svg/menu.svg" alt="" srcset="" />
+		</span>
+	{/if}
+</button>
