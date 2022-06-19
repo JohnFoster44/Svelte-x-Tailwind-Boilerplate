da2850f3b325d30cd9ee983e8359d4562423ea3e
John Foster <john@spin-up.io>
2022-06-20 00:53:21 +0100

    formats and active states

--- src/components/Modal.svelte
+++ src/components/Modal.svelte
@@ -1,16 +1,27 @@
 <script>
-    let shown = false;
-    export function toggle(){
-        shown = !shown
-    }
+	let shown = false;
+	export function toggle() {
+		shown = !shown;
+	}
 </script>
 
 {#if shown}
-<!-- Background -->
-<div class="bg-blue-500 bg-opacity-50 fixed w-full h-full top-0 left-0" on:click={() => toggle()}></div>
-<!-- Modal -->
-<div class="border-2 border-blue-500 rounded-2xl bg-slate-100 max-w-4xl p-5 mx-auto my-20 translate-y-1/2 text-center text-blue-600 container">
-    <div class="absolute right-0 mx-5"><button on:click={() => toggle()} class="rounded-full border border-blue-600 px-3.5 py-2 font-extrabold text-lg transition | hover:bg-blue-600 hover:text-green-300">&#10005;</button></div>
-    <slot>Modal Content goes here</slot>
-</div>
+	<!-- Background -->
+	<div
+		class="bg-blue-500 bg-opacity-50 fixed w-full h-full top-0 left-0"
+		on:click={() => toggle()}
+	/>
+	<!-- Modal -->
+	<div
+		class="border-2 border-blue-500 rounded-2xl bg-slate-100 max-w-4xl p-5 mx-auto my-20 translate-y-1/2 text-center text-blue-600 container"
+	>
+		<div class="absolute right-0 mx-5">
+			<button
+				on:click={() => toggle()}
+				class="rounded-full border border-blue-600 px-3.5 py-2 font-extrabold text-lg transition | hover:bg-blue-700 active:bg-blue-800 hover:text-green-300"
+				>&#10005;</button
+			>
+		</div>
+		<slot>Modal Content goes here</slot>
+	</div>
 {/if}
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,6 +1,6 @@
 <script>
-	import Modal from "../components/Modal.svelte";
-	let modal
+	import Modal from '../components/Modal.svelte';
+	let modal;
 </script>
 
 <main class="container mx-auto">
@@ -8,14 +8,17 @@
 		<h1 class="text-3xl font-bold">About page</h1>
 		<span>~</span>
 		<h3 class="font-bold mb-5">a simple page nothing much to see here</h3>
-		<button class="border p-5 bg-blue-600 text-green-300 font-bold text-lg" on:click={() => modal.toggle()}>Modal</button>
+		<button
+			class="p-5 bg-blue-600 text-green-300 font-bold text-lg rounded-xl | hover:bg-blue-700 active:bg-blue-800 hover:text-green-300"
+			on:click={() => modal.toggle()}>Modal</button
+		>
 	</div>
 </main>
 
 <Modal bind:this={modal}>
-	<h1 class="text-3xl font-bold">Modal</h1>
-	<span>~</span>
-	<p>
-		boo yahhh
-	</p>
+	<div class="mb-10">
+		<h1 class="text-3xl font-bold mb-5">Modal</h1>
+		<span>~</span>
+	</div>
+	<p>boo yahhh</p>
 </Modal>
